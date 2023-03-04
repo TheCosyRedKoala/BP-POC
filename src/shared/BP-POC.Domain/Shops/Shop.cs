54e3b6a4140f87d210d9b18db39720d1f1c94f4a
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-04 16:04:34 +0100

    Created and implemented the Shop domain class

---
+++ src/shared/BP-POC.Domain/Shops/Shop.cs
@@ -0,0 +1,42 @@
+﻿using BP_POC.Domain.Printers;
+using BP_POC.Domain.Sales;
+
+namespace BP_POC.Domain.Shops;
+
+public class Shop
+{
+    private List<Sale> _sales = new();
+    private List<Printer> _printers = new();
+
+    public string Name { get; private set; }
+    public Address Address { get; private set; }
+
+    public IReadOnlyList<Sale> Sales => _sales.AsReadOnly();
+    public IReadOnlyList<Printer> Printers => _printers.AsReadOnly();
+
+    public Shop(string name, Address address)
+    {
+        Name = Guard.Against.NullOrWhiteSpace(name, nameof(name));
+        Address = address;
+    }
+
+    public void AddPrinter(Printer printer)
+    {
+        _printers.Add(printer);
+    }
+
+    public void RemovePrinter(Printer printer)
+    {
+        _printers.Remove(printer);
+    }
+
+    public void AddSale(Sale sale) 
+    { 
+        _sales.Add(sale);
+    }
+
+    public void RemoveSale(Sale sale)
+    {
+        _sales.Remove(sale);
+    }
+}
