838c0a60b69695011e09fae9bb329e83f3b32135
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-04 17:27:23 +0100

    Created and implemented the SaleDto class

---
+++ src/management/BP-POC.Management.Shared/Sales/SaleDto.cs
@@ -0,0 +1,27 @@
+﻿using BP_POC.Management.Shared.Printers;
+
+namespace BP_POC.Management.Shared.Sales;
+
+public static class SaleDto
+{
+    public class Index
+    {
+        public int Id { get; set; }
+        public DateTime DateOfSale { get; set; }
+        public double TotalAmound { get; set; }
+    }
+
+    public class Detail
+    {
+        public int Id { get; set; }
+        public DateTime DateOfSale { get; set; }
+        public double TotalAmound { get; set; }
+        public PrinterDto.Index Printer { get; set; } = new();
+    }
+
+    public class Mutate
+    {
+        public DateTime DateOfSale { get; set; }
+        public double TotalAmound { get; set; }
+    }
+}
