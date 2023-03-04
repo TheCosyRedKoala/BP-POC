f4dfaca61872ff6a605ff377e69e9249362fc302
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-04 17:37:46 +0100

    Created and implemented the EntityNotFound exception class

---
+++ src/shared/BP-POC.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace BP_POC.Domain.Exceptions;
+
+public class EntityNotFoundException
+{
+    /// <param name="entityName">Name / type of the <see cref="Entity"/>.</param>
+    /// <param name="id">The identifier of the duplicate key.</param>
+    public EntityNotFoundException(string entityName, object id) : base($"'{entityName}' with 'Id':'{id}' was not found.")
+    {
+    }
+}
