e4190ce9831a2bbd59e1bcfda9a83af46684ba14
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-04 17:27:38 +0100

    Created and implemented the AddressDto class

---
+++ src/management/BP-POC.Management.Shared/Shops/AddressDto.cs
@@ -0,0 +1,22 @@
+﻿namespace BP_POC.Management.Shared.Shops;
+
+public static class AddressDto
+{
+    public class Index
+    {
+        public string AddressLine1 { get; set; } = default!;
+        public string? AddressLine2 { get; set; }
+        public string City { get; set; } = default!;
+        public string PostalCode { get; set; } = default!;
+        public string Country { get; set; } = default!;
+    }
+
+    public class Mutate
+    {
+        public string AddressLine1 { get; set; } = default!;
+        public string? AddressLine2 { get; set; }
+        public string City { get; set; } = default!;
+        public string PostalCode { get; set; } = default!;
+        public string Country { get; set; } = default!;
+    }
+}
