35ab542950a135341e0447747bc8466b47ad5d5a
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-04 17:26:19 +0100

    Created and implemented the PriceTierDto class

---
+++ src/management/BP-POC.Management.Shared/Printers/PriceTierDto.cs
@@ -0,0 +1,18 @@
+﻿namespace BP_POC.Management.Shared.Printers;
+
+public static class PriceTierDto
+{
+    public class Index
+    {
+        public int Floor { get; set; }
+        public int Ceiling { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Mutate
+    {
+        public int Floor { get; set; }
+        public int Ceiling { get; set; }
+        public double Price { get; set; }
+    }
+}
