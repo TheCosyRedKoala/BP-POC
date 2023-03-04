bebae0d68fc910240d555a84eceeadda4bcb4d1d
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-04 17:26:38 +0100

    Created and implemented the PrinterDto class

---
+++ src/management/BP-POC.Management.Shared/Printers/PrinterDto.cs
@@ -0,0 +1,24 @@
+﻿namespace BP_POC.Management.Shared.Printers;
+
+public static class PrinterDto
+{
+    public class Index
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+    }
+
+    public class Detail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public int ListeningInterface { get; set; }
+        public List<PriceTierDto.Index> PriceTiers { get; set; } = new();
+    }
+
+    public class Mutate
+    {
+        public string Name { get; set; } = default!;
+        public int ListeningInterface { get; set; }
+    }
+}
