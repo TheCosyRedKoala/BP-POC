fb510047301d786d7022fc46e41cdbd5d69ca293
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-04 17:27:52 +0100

    Created and implemented the ShopDto class

---
+++ src/management/BP-POC.Management.Shared/Shops/ShopDto.cs
@@ -0,0 +1,28 @@
+﻿using BP_POC.Management.Shared.Printers;
+using BP_POC.Management.Shared.Sales;
+
+namespace BP_POC.Management.Shared.Shops;
+
+public static class ShopDto
+{
+    public class Index
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+    }
+
+    public class Detail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public AddressDto.Index Address { get; set; } = new();
+        public List<SaleDto.Index> Sales { get; set; } = new();
+        public List<PrinterDto.Index> Printers { get; set; } = new();
+    }
+
+    public class Mutate
+    {
+        public string Name { get; set; }
+        public AddressDto.Mutate Address { get; set; } = new();
+    }
+}
