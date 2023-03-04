52f0aa0068665c043d5f3a173a080f19629c8b79
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-04 15:49:29 +0100

    Adjusted PriceTier.Price property to be a double

--- src/shared/BP-POC.Domain/Printers/PriceTier.cs
+++ src/shared/BP-POC.Domain/Printers/PriceTier.cs
@@ -4,9 +4,9 @@ public class PriceTier
 {
     public int Floor { get; private set; }
     public int Ceiling { get; private set; }
-    public decimal Price { get; private set; }
+    public double Price { get; private set; }
 
-    public PriceTier(int floor, int ceiling, decimal price)
+    public PriceTier(int floor, int ceiling, double price)
     {
         Floor = Guard.Against.Negative(floor, nameof(floor));
         Ceiling = Guard.Against.NegativeOrZero(ceiling, nameof(ceiling));
