96ad15dfbf4a6044f7859736316d08d5cb66c9f0
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-04 17:28:07 +0100

    Created the IShopService interface

---
+++ src/management/BP-POC.Management.Shared/Shops/IShopService.cs
@@ -0,0 +1,21 @@
+﻿using BP_POC.Management.Shared.Printers;
+using BP_POC.Management.Shared.Sales;
+
+namespace BP_POC.Management.Shared.Shops;
+
+public interface IShopService
+{
+    // Shop
+    Task CreateAsync();
+    Task GetIndexAsync();
+    Task GetDetailAsync();
+
+    // Printer
+    Task<int> CreatePrinterAsync(PrinterDto.Mutate model);
+    Task DeletePrinterAsync(int id);
+
+    // Sale
+    Task<int> CreateSaleAsync(SaleDto.Mutate model);
+    Task DeleteSaleAsync(int id);
+
+}
