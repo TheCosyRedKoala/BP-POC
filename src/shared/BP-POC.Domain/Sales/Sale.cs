18a539254dd0ba834023676ee84b77c7225a13d9
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-04 16:04:00 +0100

    Created and implemented the Sale domain class

---
+++ src/shared/BP-POC.Domain/Sales/Sale.cs
@@ -0,0 +1,17 @@
+﻿using BP_POC.Domain.Printers;
+
+namespace BP_POC.Domain.Sales;
+
+public class Sale
+{
+    public DateTime DateOfSale { get; private set; }
+    public double TotalAmount { get; }
+    public Printer Printer { get; }
+
+    public Sale(double totalAmount, Printer printer)
+    {
+        TotalAmount = Guard.Against.NegativeOrZero(totalAmount, nameof(totalAmount));
+        Printer = Guard.Against.Null(printer, nameof(printer));
+        DateOfSale = DateTime.UtcNow;
+    }
+}
