1de110806b1211fa32a73281121847c2b4c60e72
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-05 11:22:31 +0100

    Created and implemented the PrinterConfiguration class

---
+++ src/shared/BP-POC.Persistence/Configurations/Printers/PrinterConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using BP_POC.Domain.Printers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BP_POC.Persistence.Configurations.Printers;
+
+internal class PrinterConfiguration : EntityConfiguration<Printer>
+{
+    public override void Configure(EntityTypeBuilder<Printer> builder)
+    {
+        base.Configure(builder);
+
+        builder.HasMany(p => p.PriceTiers)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
