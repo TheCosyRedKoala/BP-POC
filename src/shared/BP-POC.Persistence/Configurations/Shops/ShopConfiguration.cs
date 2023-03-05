25fd6c80b718691e988bc7542c3df0a631cbae40
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-05 11:23:03 +0100

    Created and implemented the ShopConfiguration class

---
+++ src/shared/BP-POC.Persistence/Configurations/Shops/ShopConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using BP_POC.Domain.Shops;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BP_POC.Persistence.Configurations.Shops;
+
+internal class ShopConfiguration : EntityConfiguration<Shop>
+{
+    public override void Configure(EntityTypeBuilder<Shop> builder)
+    {
+        builder.HasMany(s => s.Sales)
+            .WithOne();
+
+        builder.HasMany(s => s.Printers)
+            .WithOne();
+
+        builder.OwnsOne(s => s.Address, address =>
+        {
+            address.Property(a => a.AddressLine1)
+                .HasColumnName(nameof(Address.AddressLine1));
+
+            address.Property(a => a.AddressLine2)
+                .HasColumnName(nameof(Address.AddressLine2));
+
+            address.Property(a => a.City)
+                .HasColumnName(nameof(Address.City));
+
+            address.Property(a => a.PostalCode)
+                .HasColumnName(nameof(Address.PostalCode));
+
+            address.Property(a => a.Country)
+                .HasColumnName(nameof(Address.Country));
+        });
+    }
+}
