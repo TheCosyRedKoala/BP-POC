e4d83d529a471d884358301e2d42cd899555db24
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 20:46:48 +0100

    Generated BP-POC.Operations.Client project

---
+++ src/operations/BP-POC.Operations.Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BP_POC.Operations.Client
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/operations/BP-POC.Operations.Client/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BP_POC.Operations.Client
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
