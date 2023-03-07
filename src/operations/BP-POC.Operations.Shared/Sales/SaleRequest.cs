99947fa8b69096c64a0939953f9ee1bd5da8068e
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 20:28:08 +0100

    Created and implemented the SaleRequest class

---
+++ src/operations/BP-POC.Operations.Shared/Sales/SaleRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace BP_POC.Operations.Shared.Sales;
+
+public static class SaleRequest
+{
+    public class Mutate
+    {
+        public int ShopId { get; set; }
+        public int PrinterId { get; set; }
+        public SaleDto.Mutate Sale { get; set; } = new();
+    }
+}
