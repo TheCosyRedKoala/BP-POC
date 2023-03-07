d3ede67b1f073f3b2c7325a6fd766f25c81d9ec2
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 19:42:18 +0100

    Added new migrations

---
+++ src/shared/BP-POC.Persistence/Migrations/20230307184152_FinishedManagementApi.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BP_POC.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinishedManagementApi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shop",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false),
+                    AddressLine1 = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false),
+                    AddressLine2 = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: true),
+                    City = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    IsEnabled = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shop", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Printer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false),
+                    ListeningInterface = table.Column<int>(type: "int", nullable: false),
+                    ShopId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    IsEnabled = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Printer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Printer_Shop_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "Shop",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sale",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateOfSale = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ShopId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    IsEnabled = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sale", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sale_Shop_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "Shop",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PriceTier",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Floor = table.Column<int>(type: "int", nullable: false),
+                    Ceiling = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    PrinterId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    IsEnabled = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PriceTier", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PriceTier_Printer_PrinterId",
+                        column: x => x.PrinterId,
+                        principalTable: "Printer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PriceTier_PrinterId",
+                table: "PriceTier",
+                column: "PrinterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Printer_ShopId",
+                table: "Printer",
+                column: "ShopId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sale_ShopId",
+                table: "Sale",
+                column: "ShopId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PriceTier");
+
+            migrationBuilder.DropTable(
+                name: "Sale");
+
+            migrationBuilder.DropTable(
+                name: "Printer");
+
+            migrationBuilder.DropTable(
+                name: "Shop");
+        }
+    }
+}
