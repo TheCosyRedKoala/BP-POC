f4fec6dac9f4cab9aae06e09067d585e967d2f6c
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 20:33:34 +0100

    Created and implemented the SaleController class

---
+++ src/operations/BP-POC.Operations.Api/Controllers/SaleController.cs
@@ -0,0 +1,34 @@
+﻿using BP_POC.Operations.Shared.Sales;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BP_POC.Operations.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SaleController : ControllerBase
+{
+    private readonly ISaleService _saleService;
+
+    public SaleController(ISaleService printerService)
+    {
+        _saleService = printerService;
+    }
+
+    [HttpPost]
+    public async Task<int> CreateAsync([FromBody] SaleRequest.Mutate request)
+    {
+        return await _saleService.CreateAsync(request);
+    }
+
+    [HttpGet]
+    public async Task<List<SaleDto.Index>> GetIndexAsync()
+    {
+        return await _saleService.GetIndexAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<SaleDto.Detail> GetByIdAsync(int id)
+    {
+        return await _saleService.GetByIdAsync(id);
+    }
+}
