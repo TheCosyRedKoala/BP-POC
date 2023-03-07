4dacc9571aa306c9f61c0f07f3dfdad5db9de342
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 19:22:30 +0100

    Removed unreferenced inner classes

--- src/management/BP-POC.Management.Shared/Sales/SaleDto.cs
+++ src/management/BP-POC.Management.Shared/Sales/SaleDto.cs
@@ -10,18 +10,4 @@ public static class SaleDto
         public DateTime DateOfSale { get; set; }
         public double TotalAmount { get; set; }
     }
-
-    public class Detail
-    {
-        public int Id { get; set; }
-        public DateTime DateOfSale { get; set; }
-        public double TotalAmount { get; set; }
-        public PrinterDto.Index Printer { get; set; } = new();
-    }
-
-    public class Mutate
-    {
-        public DateTime DateOfSale { get; set; }
-        public double TotalAmount { get; set; }
-    }
 }
