87c746fca9b83adf1973b7a03608b7cb41344862
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 20:32:01 +0100

    Created and implemented the SaleService class

---
+++ src/operations/BP-POC.Operations.Services/Sales/SaleService.cs
@@ -0,0 +1,83 @@
+﻿using BP_POC.Domain.Exceptions;
+using BP_POC.Domain.Printers;
+using BP_POC.Domain.Sales;
+using BP_POC.Domain.Shops;
+using BP_POC.Operations.Shared.Printers;
+using BP_POC.Operations.Shared.Sales;
+using BP_POC.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace BP_POC.Operations.Services.Sales;
+
+public class SaleService : ISaleService
+{
+    private readonly ApplicationDbContext _context;
+
+    public SaleService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> CreateAsync(SaleRequest.Mutate request)
+    {
+        Shop? shop = await _context.Shops
+            .FirstOrDefaultAsync(s => request.ShopId == s.Id);
+
+        if (shop is null)
+        {
+            throw new EntityNotFoundException(nameof(Shop), request.ShopId);
+        }
+
+        Printer? printer = await _context.Printers
+            .FirstOrDefaultAsync(p => request.PrinterId == p.Id);
+
+        if (printer is null) 
+        {
+            throw new EntityNotFoundException(nameof(Printer), request.PrinterId);
+        }
+
+        Sale sale = new(request.Sale.TotalAmount, printer);
+
+        shop.AddSale(sale);
+
+        await _context.SaveChangesAsync();
+
+        return sale.Id;
+    }
+
+    public async Task<List<SaleDto.Index>> GetIndexAsync()
+    {
+        return await _context.Sales
+            .Select(s => new SaleDto.Index
+            {
+                Id = s.Id,
+                DateOfSale= s.DateOfSale,
+                TotalAmount = s.TotalAmount
+            })
+            .ToListAsync();
+    }
+
+    public async Task<SaleDto.Detail> GetByIdAsync(int id)
+    {
+        Sale? sale = await _context.Sales
+            .Include(s => s.Printer)
+            .FirstOrDefaultAsync(s => id == s.Id);
+
+        if (sale is null)
+        {
+            throw new EntityNotFoundException(nameof(Sale), id);
+        }
+
+        return new SaleDto.Detail
+        {
+            Id = sale.Id,
+            DateOfSale = sale.DateOfSale,
+            TotalAmount = sale.TotalAmount,
+            Printer = new PrinterDto.Index
+            {
+                Id = sale.Printer.Id,
+                Name = sale.Printer.Name
+            }
+        };
+    }
+}
