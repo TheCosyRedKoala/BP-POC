b46c9961ae822e24b4dbb62dfea1f09620ee2e1a
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 20:30:05 +0100

    Created and implemented the IPrinterService interface

---
+++ src/operations/BP-POC.Operations.Shared/Printers/IPrinterService.cs
@@ -0,0 +1,8 @@
+﻿namespace BP_POC.Operations.Shared.Printers;
+
+public interface IPrinterService
+{
+    Task<List<PrinterDto.Index>> GetIndexAsync();
+    Task<List<PrinterDto.Index>> GetByShopIdAsync(int id);
+    Task<PrinterDto.Detail> GetByIdAsync(int id);
+}
