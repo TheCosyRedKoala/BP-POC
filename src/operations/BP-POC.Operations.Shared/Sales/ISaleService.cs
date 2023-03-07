c5904dc99fbf6e1a24603a558c3ab4ab88bcb8d1
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 20:30:40 +0100

    Created and implemented the ISaleService interface

---
+++ src/operations/BP-POC.Operations.Shared/Sales/ISaleService.cs
@@ -0,0 +1,8 @@
+﻿namespace BP_POC.Operations.Shared.Sales;
+
+public interface ISaleService
+{
+    Task<int> CreateAsync(SaleRequest.Mutate request);
+    Task<List<SaleDto.Index>> GetIndexAsync();
+    Task<SaleDto.Detail> GetByIdAsync(int id);
+}
