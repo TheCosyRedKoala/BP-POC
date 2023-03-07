bde3c8801ac6bd65c6ee426bb8382c52c8869a8d
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 20:32:53 +0100

    Generated the BP-POC.Operations.Api projected

---
+++ src/operations/BP-POC.Operations.Api/Program.cs
@@ -0,0 +1,45 @@
+using BP_POC.Operations.Services;
+using BP_POC.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+    // Add controllers
+builder.Services.AddControllers();
+    // Add REST services
+builder.Services.AddRestServices();
+
+// Swagger | OAS
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.CustomSchemaIds(type => type.DeclaringType is null ? $"{type.Name}" : $"{type.DeclaringType?.Name}.{type.Name}");
+    options.EnableAnnotations();
+});
+
+// Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer
+    (
+        builder.Configuration.GetConnectionString("SqlServer")
+    );
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
