22eca1cca7fee8835abbd74bd73e07dc41251e4b
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 19:06:35 +0100

    Created and implemented PriceTierConfiguration class

---
+++ src/shared/BP-POC.Persistence/Configurations/Printers/PriceTierConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using BP_POC.Domain.Printers;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BP_POC.Persistence.Configurations.Printers;
+
+internal class PriceTierConfiguration : EntityConfiguration<PriceTier>
+{
+    public override void Configure(EntityTypeBuilder<PriceTier> builder)
+    {
+        base.Configure(builder);
+    }
+}
