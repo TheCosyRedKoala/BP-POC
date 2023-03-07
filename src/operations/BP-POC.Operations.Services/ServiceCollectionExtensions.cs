fd134f0b05baae8bb2864f086281f56ba3d91934
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 20:32:16 +0100

    Created and implemented the ServiceCollectionExtensions class

---
+++ src/operations/BP-POC.Operations.Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using BP_POC.Operations.Services.Printers;
+using BP_POC.Operations.Services.Sales;
+using BP_POC.Operations.Shared.Printers;
+using BP_POC.Operations.Shared.Sales;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BP_POC.Operations.Services;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds all services to the DI container.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddRestServices(this IServiceCollection services)
+    {
+        services.AddScoped<IPrinterService, PrinterService>();
+        services.AddScoped<ISaleService, SaleService>();
+
+        return services;
+    }
+}
