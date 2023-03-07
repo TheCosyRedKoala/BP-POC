b25b5289e576d5c5e405c970f20c531555a27bd3
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 19:06:46 +0100

    Created and implemented SaleConfiguration class

---
+++ src/shared/BP-POC.Persistence/Configurations/Sales/SaleConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using BP_POC.Domain.Sales;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BP_POC.Persistence.Configurations.Sales;
+
+internal class SaleConfiguration : EntityConfiguration<Sale>
+{
+    public override void Configure(EntityTypeBuilder<Sale> builder)
+    {
+        base.Configure(builder);
+    }
+}
