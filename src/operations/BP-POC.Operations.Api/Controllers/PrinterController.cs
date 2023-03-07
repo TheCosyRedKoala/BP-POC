42f6fab1ce914714874e2f691c6033ebb23854d8
Michiel Van Herreweghe <michiel.vanherreweghe@outlook.com>
2023-03-07 20:33:21 +0100

    Created and implemented the PrinterController class

---
+++ src/operations/BP-POC.Operations.Api/Controllers/PrinterController.cs
@@ -0,0 +1,35 @@
+﻿using BP_POC.Operations.Shared.Printers;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace BP_POC.Operations.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PrinterController : ControllerBase
+{
+    private readonly IPrinterService _printerService;
+
+	public PrinterController(IPrinterService printerService)
+	{
+		_printerService = printerService;
+	}
+
+    [HttpGet]
+    public async Task<List<PrinterDto.Index>> GetIndexAsync()
+    {
+        return await _printerService.GetIndexAsync();
+    }
+
+    [HttpGet("Shop/{id}")]
+    public async Task<List<PrinterDto.Index>> GetByShopIdAsync(int id)
+    {
+        return await _printerService.GetByShopIdAsync(id);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<PrinterDto.Detail> GetDetailAsync(int id)
+    {
+        return await _printerService.GetByIdAsync(id);
+    }
+}
